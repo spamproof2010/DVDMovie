3eb5064fd5e174266ae6c7f239de5be597e07fbe
unknown <reynald.consultant@gmail.com>
2019-01-23 20:28:09 -0800

    3.1 Creating MovieTable Component

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -4,9 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ModelModule } from "./models/model.module";
+import { MovieTableComponent } from "./structure/movieTable.component"
+import { CategoryFilterComponent } from "./structure/categoryFilter.component"
+
 
 @NgModule({
   declarations: [
+    MovieTableComponent, 
+    CategoryFilterComponent,
     AppComponent
   ],
   imports: [
