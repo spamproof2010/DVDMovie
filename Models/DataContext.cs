5aa90f775bd619d9cdfa3a80463f134733f2327f
unknown <reynald.consultant@gmail.com>
2019-01-25 23:42:45 -0800

    5.5 Checkout Process Pt 1

--- Models/DataContext.cs
+++ Models/DataContext.cs
@@ -7,6 +7,7 @@ public DataContext(DbContextOptions<DataContext> opts)
     public DbSet<Movie> Movies {get; set;}
     public DbSet<Studio> Studios { get; set; }
     public DbSet<Rating> Ratings { get; set; }
+    public DbSet<Order> Orders { get; set; }
 
      protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
